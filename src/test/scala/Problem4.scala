b0e7be9da0372cef57f5b439f4ec911347802c66
Michael Hughes <michaelhughes@Michael-Hughess-MacBook-Pro.local>
2012-03-24 21:15:08 +0000

    Initial commit of problem4

---
+++ src/test/scala/Problem4.scala
@@ -0,0 +1,31 @@
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import scala.annotation.tailrec
+
+object Problem4 {
+  def palindromes(max : Int) = {
+    (for {
+      outer <- 2 to max
+      inner <- 1 to (outer - 1) 
+      val num = outer*inner
+      if isPalindrome(num.toString())
+    } yield num
+    ).sortWith((a,b) => a<b)
+  }
+  
+  @tailrec
+  def isPalindrome(characters : String, position:Int = 0) : Boolean = 
+    if(position > characters.size/2) true
+    else if(characters(position) != characters((characters.size-position)-1)) false
+    else isPalindrome(characters, position + 1)
+}
+
+class Problem4Tests extends FunSuite with ShouldMatchers {
+  test("The largest palindrome from a 2 digit product should be 9009") {
+    Problem4.palindromes(99).last should be (9009)
+  }
+  
+  test("The largest palindrome from a 3 digit product should be 906609") {
+    Problem4.palindromes(999).last should be (906609)
+  }
+}
